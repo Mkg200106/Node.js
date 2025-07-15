f8d9d7ac02b3d5ddbf5978dc75e80b8ff760123c
Martha404 <martharamphele04@gmail.com>
2025-07-15 11:00:24 -0700

    Lesson 5

--- Unit.1/Lesson 4/simple_server/main.js
+++ Unit.1/Lesson 4/simple_server/main.js
@@ -1,16 +1,23 @@
 const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
+  
+  // Creating the HTTP server
   app = http.createServer((request, response) => {
     console.log("Received an incoming request!");
+    
+    // Writing the HTTP header with status 200 and content type HTML
     response.writeHead(httpStatus.OK, {
       "Content-Type": "text/html"
     });
+    
+    // Creating and sending the HTML response
     let responseMessage = "<h1>Hello, Universe!</h1>";
     response.write(responseMessage);
     response.end();
     console.log(`Sent a response : ${responseMessage}`);
  });
 
+// Start the server
 app.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
